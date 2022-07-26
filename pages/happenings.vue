b9b23780dbb55120f64444b8ced3ea81be4b5776
kelvin.beak <brightsky16@gmail.com>
2022-07-27 00:16:59 +0800

    assets

---
+++ pages/happenings.vue
@@ -0,0 +1,379 @@
+<template>
+  <div
+    class="md:py-[40px] md:pl-[40px] lg:pr-[30px] sm:px-[24px] py-[24px] lg:py-[56px] lg:pl-[64px] lg:flex-1 lg:overflow-hidden mb-[24px]"
+  >
+    <section class="px-[24px] sm:px-0">
+      <h1
+        class="text-[32px] leading-[40px] sm:text-[48px] sm:leading-[64px] font-bold mb-[4px] text-[#0B0C0F]"
+      >
+        Happenings
+      </h1>
+      <p class="text-[16px] leading-[24px] text-[#21252C]">
+        Stay in the know about the latest happenings & buzz in church!
+      </p>
+    </section>
+
+    <section v-if="filteredUpcomingEvents.length > 0" class="sm:mt-[56px] mt-[24px]">
+        <h1 class="font-bold text-xl px-[24px] sm:px-0">Upcoming events</h1>
+
+      <!-- <Slider
+        class="mt-[24px]"
+        :class="{
+          'lg:hidden': filteredUpcomingEvents.length < 4,
+        }"
+        :items="filteredUpcomingEvents"
+        :slides-per-view="1.5"
+        :breakpoints="breakpoints"
+        blur-color="#F8F9F9"
+        has-arrows-desktop
+        has-pagination
+        custom
+      > -->
+      <div :v-for="item in filteredUpcomingEvents">
+        <template>
+          <CardTile
+            class="h-full border-[0px] rounded-lg drop-shadow-tile relative flex flex-col"
+            corner-text-class="text-xs lg:text-[16px] sm:px-[12px] font-semibold leading-[20px]"
+            :image-src="item.imageSrc"
+            :corner-text="item.cornerText"
+            text-wrapper-class="min-h-[48px] text-left pt-[20px] px-[20px] pb-[72px]"
+          >
+            <h1 class="text-md font-bold sm:text-[20px] sm:leading-[24px]">
+              {{ item.title }}
+            </h1>
+
+            <p
+              class="pt-[8px] pb-[12px] text-md leading-6 sm:leading-[24px] whitespace-pre-line mb-auto"
+            >
+              {{ item.body }}
+            </p>
+            <a
+              v-if="item.isExternal"
+              :href="item.to"
+              target="_blank"
+              :data-event-tracking="item.dataEventTracking"
+            >
+              <BaseButton
+                class="absolute bottom-[20px]"
+                solid-classes="bg-[#6245F4] text-white text-xs border-0 hover:bg-[#3B4CEF] sm:text-[16px] sm:leading-[24px]"
+                :data-event-tracking="item.dataEventTracking"
+              >
+                {{ item.buttonText }}
+              </BaseButton>
+            </a>
+            <nuxt-link v-else :to="item.to" :data-event-tracking="item.dataEventTracking">
+              <BaseButton
+                class="absolute bottom-[20px]"
+                solid-classes="bg-[#6245F4] text-white text-xs border-0 hover:bg-[#3B4CEF] sm:text-[16px] sm:leading-[24px]"
+                :data-event-tracking="item.dataEventTracking"
+              >
+                {{ item.buttonText }}
+              </BaseButton>
+            </nuxt-link>
+          </CardTile>
+        </template>
+        </div>
+      <!-- </Slider> -->
+
+      <div v-if="filteredUpcomingEvents.length < 4" class="hidden mt-4 lg:flex lg:items-stretch">
+        <CardTile
+          v-for="(item, i) in filteredUpcomingEvents"
+          :key="i"
+          class="desktop-slide flex-1 border-[0px] rounded-lg drop-shadow-tile relative"
+          :class="{
+            'tablet-slide': filteredUpcomingEvents.length < 3,
+            'ml-[20px]': i >= 1,
+          }"
+          corner-text-class="text-xs lg:text-[16px] sm:px-[12px] font-semibold leading-[20px]"
+          :image-src="item.imageSrc"
+          :corner-text="item.cornerText"
+          text-wrapper-class="text-left pt-[20px] px-[20px] pb-[72px]"
+        >
+          <h1 class="text-md font-bold sm:text-[20px] sm:leading-[24px]">
+            {{ item.title }}
+          </h1>
+
+          <p
+            class="pt-[8px] pb-[12px] text-md leading-6 sm:leading-[24px] whitespace-pre-line mb-auto"
+          >
+            {{ item.body }}
+          </p>
+
+          <a
+            v-if="item.isExternal"
+            :href="item.to"
+            target="_blank"
+            :data-event-tracking="item.dataEventTracking"
+          >
+            <BaseButton
+              class="absolute bottom-[20px]"
+              solid-classes="bg-[#6245F4] text-white text-xs border-0 hover:bg-[#3B4CEF] sm:text-[16px] sm:leading-[24px]"
+              :data-event-tracking="item.dataEventTracking"
+            >
+              {{ item.buttonText }}
+            </BaseButton>
+          </a>
+
+          <nuxt-link v-else :to="item.to" :data-event-tracking="item.dataEventTracking">
+            <BaseButton
+              class="absolute bottom-[20px]"
+              solid-classes="bg-[#6245F4] text-white text-xs border-0 hover:bg-[#3B4CEF] sm:text-[16px] sm:leading-[24px]"
+              :data-event-tracking="item.dataEventTracking"
+            >
+              {{ item.buttonText }}
+            </BaseButton>
+          </nuxt-link>
+        </CardTile>
+      </div>
+    </section>
+    <!-- </div> -->
+
+    <section v-if="filteredWeeklyEvents.length > 0" class="mt-16">
+      <h1 class="font-bold text-xl px-[24px] sm:px-0">Weekly events</h1>
+
+      <!-- <Slider
+        class="mt-[24px]"
+        :class="{
+          'lg:hidden': filteredWeeklyEvents.length < 4,
+        }"
+        :items="filteredWeeklyEvents"
+        :slides-per-view="1.5"
+        :breakpoints="breakpoints"
+        blur-color="#F8F9F9"
+        has-arrows-desktop
+        has-pagination
+        custom
+      > -->
+      <div :v-for="item in filteredWeeklyEvents">
+        <template>
+          <CardTile
+            class="h-full border-[0px] rounded-lg drop-shadow-tile relative flex flex-col"
+            corner-text-class="text-xs lg:text-[16px] sm:px-[12px] font-semibold leading-[20px]"
+            :image-src="item.imageSrc"
+            :corner-text="item.cornerText"
+            text-wrapper-class="text-left pt-[20px] px-[20px] pb-[72px]"
+          >
+            <h1 class="min-h-[48px] text-md font-bold sm:text-[20px] sm:leading-[24px]">
+              {{ item.title }}
+            </h1>
+
+            <p
+              class="pt-[8px] pb-[12px] text-md leading-6 sm:leading-[24px] whitespace-pre-line mb-auto"
+            >
+              {{ item.body }}
+            </p>
+
+            <a
+              v-if="item.isExternal"
+              :href="item.to"
+              target="_blank"
+              :data-event-tracking="item.dataEventTracking"
+            >
+              <BaseButton
+                class="absolute bottom-[20px]"
+                solid-classes="bg-[#6245F4] text-white text-xs border-0 hover:bg-[#3B4CEF] sm:text-[16px] sm:leading-[24px]"
+                :data-event-tracking="item.dataEventTracking"
+              >
+                {{ item.buttonText }}
+              </BaseButton>
+            </a>
+
+            <nuxt-link v-else :to="item.to" :data-event-tracking="item.dataEventTracking">
+              <BaseButton
+                class="absolute bottom-[20px]"
+                solid-classes="bg-[#6245F4] text-white text-xs border-0 hover:bg-[#3B4CEF] sm:text-[16px] sm:leading-[24px]"
+                :data-event-tracking="item.dataEventTracking"
+              >
+                {{ item.buttonText }}
+              </BaseButton>
+            </nuxt-link>
+          </CardTile>
+        </template>
+        </div>
+      <!-- </Slider> -->
+
+      <div v-if="filteredWeeklyEvents.length < 4" class="hidden mt-4 lg:flex lg:items-stretch">
+        <CardTile
+          v-for="(item, i) in filteredWeeklyEvents"
+          :key="i"
+          class="desktop-slide flex-1 border-[0px] rounded-lg drop-shadow-tile relative"
+          :class="{
+            'tablet-slide': filteredWeeklyEvents.length < 4,
+            'ml-[20px]': i >= 1,
+          }"
+          corner-text-class="text-xs lg:text-[16px] sm:px-[12px] font-semibold leading-[20px]"
+          :image-src="item.imageSrc"
+          text-wrapper-class="text-left pt-[20px] px-[20px] pb-[72px]"
+        >
+          <h1 class="min-h-[48px] text-md font-bold sm:text-[20px] sm:leading-[24px]">
+            {{ item.title }}
+          </h1>
+
+          <p
+            class="pt-[8px] pb-[12px] text-md leading-6 sm:leading-[24px] whitespace-pre-line mb-auto"
+          >
+            {{ item.body }}
+          </p>
+
+          <a
+            v-if="item.isExternal"
+            :href="item.to"
+            target="_blank"
+            :data-event-tracking="item.dataEventTracking"
+          >
+            <BaseButton
+              class="absolute bottom-[20px]"
+              solid-classes="bg-[#6245F4] text-white text-xs border-0 hover:bg-[#3B4CEF] sm:text-[16px] sm:leading-[24px]"
+              :data-event-tracking="item.dataEventTracking"
+            >
+              {{ item.buttonText }}
+            </BaseButton>
+          </a>
+
+          <nuxt-link v-else :to="item.to" :data-event-tracking="item.dataEventTracking">
+            <BaseButton
+              class="absolute bottom-[20px]"
+              solid-classes="bg-[#6245F4] text-white text-xs border-0 hover:bg-[#3B4CEF] sm:text-[16px] sm:leading-[24px]"
+              :data-event-tracking="item.dataEventTracking"
+            >
+              {{ item.buttonText }}
+            </BaseButton>
+          </nuxt-link>
+        </CardTile>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import BaseButton from '../components/BaseButton.vue'
+import CardTile from '../components/CardTile.vue'
+// { BaseButton, Slider, CardTile } from '@team-hogc/design-internal-components-vue'
+
+export default {
+  components: {
+    // Slider,
+    CardTile,
+    BaseButton,
+  },
+
+  data () {
+    return {
+      upcomingEvents: [
+      ],
+      weeklyEvents: [
+        {
+          imageSrc: require('~/assets/images/happenings/heartkidz.png'),
+          title: "Children's Church Registration",
+          body: 'Every Sat 5-7pm & Sun 1.30-3.30pm',
+          buttonText: 'Find out more',
+          to: 'happenings/hkdz-registration',
+          isExternal: true,
+          dataEventTracking: 'childrens-church-registration-happenings',
+        },
+        {
+          imageSrc: require('~/assets/images/happenings/service-replay.png'),
+          title: 'Service Replay',
+          body: `No FOMO!
+          Sun 1.30pm - 3.30pm`,
+          buttonText: 'Find out more',
+          to: 'happenings/service-replay',
+          maxPastoralStatus: 3,
+          isExternal: true,
+          dataEventTracking: 'service-replay-interest-form',
+        },
+        {
+          imageSrc: require('~/assets/images/happenings/service-replay.png'),
+          title: 'Service Replay',
+          body: `No FOMO!
+          Sun 1.30pm - 3.30pm`,
+          buttonText: 'Find out more',
+          to: 'https://one-page.heartofgodchurch.org/videos',
+          minPastoralStatus: 4,
+          isExternal: true,
+          dataEventTracking: 'service-replay-interest-form',
+        },
+        {
+          imageSrc: require('~/assets/images/happenings/youth-connect.jpg'),
+          title: 'Connect a Youth',
+          body: 'Bringing a youth? Let us know! We’re here to help!',
+          buttonText: 'Find out more',
+          to: '/happenings/hogc21-connect',
+          minPastoralStatus: 4,
+          isExternal: true,
+          dataEventTracking: 'hogc21-connect-form',
+        },
+        {
+          imageSrc: require('~/assets/images/happenings/youth-involvement.jpg'),
+          title: 'Get Involved',
+          body: 'We need youth & we need YOU! Let’s reach down!',
+          buttonText: 'Find out more',
+          to: '/happenings/hogc21-involvement',
+          minPastoralStatus: 4,
+          isExternal: true,
+          dataEventTracking: 'hogc21-involvement-form',
+        },
+        {
+          imageSrc: require('~/assets/images/happenings/hogc21.png'),
+          title: 'HOGC21 Ideas',
+          body: 'Want to do something but not sure how? Fret not!',
+          buttonText: 'Find out more',
+          to: 'https://d1ocj872pzimwq.cloudfront.net/HOGC21/HOGC21+Involvement+Ideas.pdf?name=HOGC21%20Involvement%20Brochure',
+          minPastoralStatus: 4,
+          isExternal: true,
+          dataEventTracking: 'hogc21-involvement-brochure',
+        },
+        {
+          imageSrc: require('~/assets/images/happenings/youthevents.jpg'),
+          title: 'Youth Events',
+          body: 'Calling all who are young at heart!',
+          buttonText: 'Find out more',
+          to: '/happenings/youth-events',
+          minPastoralStatus: 4,
+          isExternal: true,
+          dataEventTracking: 'youth-events-interest-form',
+        },
+      ],
+    }
+  },
+
+  computed: {
+    breakpoints () {
+      return {
+        641: {
+          slidesPerView: 2,
+          centeredSlides: false,
+        },
+        1024: {
+          slidesPerView: 3,
+          centeredSlides: false,
+        },
+        1366: {
+          slidesPerView: 4,
+          centeredSlides: false,
+        },
+      }
+    },
+    filteredUpcomingEvents () {
+      return this.upcomingEvents.filter(c => (!c.minPastoralStatus && !c.maxPastoralStatus) || this.pastoralMapping[this.pastoralRoleId] >= c.minPastoralStatus || this.pastoralMapping[this.pastoralRoleId] <= c.maxPastoralStatus)
+    },
+    filteredWeeklyEvents () {
+      return this.weeklyEvents.filter(c => (!c.minPastoralStatus && !c.maxPastoralStatus) || this.pastoralMapping[this.pastoralRoleId] >= c.minPastoralStatus || this.pastoralMapping[this.pastoralRoleId] <= c.maxPastoralStatus)
+    },
+  },
+}
+</script>
+
+<style scoped>
+@media (min-width: 1024px) {
+  .tablet-slide {
+    max-width: calc(33.3% - 15px);
+  }
+}
+
+@media (min-width: 1366px) {
+  .desktop-slide {
+    max-width: calc(25% - 15px);
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,21 @@
 <template>
-
-<div>
-  "hello world"
-</div>
+  <div class="lg:pr-[30px] lg:flex-1 lg:overflow-hidden relative">
+    <!-- <ButtonsCard class="px-[24px] lg:pl-[49px] lg:pr-0" />
+    <div>
+      <WeekendHighlights class="mt-[90px] lg:mt-[64px]" />
+      <JustForYou class="px-[24px] mt-[48px] mb-[58px] lg:pl-[49px] lg:pr-0" /> -->
+    <!-- </div> -->
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'IndexPage'
-}
+// import ButtonsCard from '../components/HomePage/ButtonsCard.vue'
+// import WeekendHighlights from '@/components/HomePage/WeekendHighlights.vue'
+// import JustForYou from '@/components/HomePage/JustForYou.vue'
+
+// export default {
+//   components: { WeekendHighlights, JustForYou, ButtonsCard },
+//   layout: 'default',
+// }
+
 </script>
